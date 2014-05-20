1ff8dc9e76b06c96ac95f89765b1fef84ca40237
mpall <marcuspallot@hotmail.com>
2014-05-20 21:13:38 +0100

    Now at start of templates section

--- chef.sh
+++ chef.sh
@@ -146,3 +146,179 @@ echo "
 <?php phpinfo(); ?>
 " > /var/www/test.php
 
+knife cookbook site download database
+tar zxf database-*.tar.gz
+knife cookbook site download postgresql
+tar zxf postgresql-*.tar.gz
+knife cookbook site download xfs
+tar zxf xfs-*.tar.gz
+knife cookbook site download aws
+tar zxf aws-*.tar.gz
+rm *.tar.gz
+knife cookbook site download mysql-chef_gem
+tar zxf mysql-chef_gem-*.tar.gz
+rm *.tar.gz
+
+
+echo "depends \"database\"" >> phpapp/metadata.rb
+
+echo "
+#
+# Cookbook Name:: phpapp
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+include_recipe \"apache2\"
+include_recipe \"mysql::client\"
+include_recipe \"mysql::server\"
+include_recipe \"php\"
+include_recipe \"php::module_mysql\"
+include_recipe \"apache2::mod_php5\"
+include_recipe \"mysql::ruby\"
+
+apache_site \"default\" do
+  enable true
+end
+
+mysql_database node['phpapp']['database'] do
+  connection ({:host => 'localhost', :username => 'root', :password => node['mysql']['server_root_password']})
+  action :create
+end 
+" > phpapp/recipes/default.rb
+
+
+echo "
+default[\"phpapp\"][\"database\"] = \"phpapp\"
+" > phpapp/attributes/default.rb
+
+#sudo netstat -tap | grep mysql
+
+
+echo "
+#
+# Cookbook Name:: phpapp
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+include_recipe \"apache2\"
+include_recipe \"mysql::client\"
+include_recipe \"mysql::server\"
+include_recipe \"php\"
+include_recipe \"php::module_mysql\"
+include_recipe \"apache2::mod_php5\"
+include_recipe \"mysql::ruby\"
+
+apache_site \"default\" do
+  enable true
+end
+
+mysql_database node['phpapp']['database'] do
+  connection ({:host => 'localhost', :username => 'root', :password => node['mysql']['server_root_password']})
+  action :create
+end
+
+mysql_database_user node['phpapp']['db_username'] do
+  connection ({:host => 'localhost', :username => 'root', :password => node['mysql']['server_root_password']})
+  password node['phpapp']['db_password']
+  database_name node['phpapp']['database']
+  privileges [:select,:update,:insert,:create,:delete]
+  action :grant
+end
+ 
+" > phpapp/recipes/default.rb
+
+echo "
+default[\"phpapp\"][\"database\"] = \"phpapp\"
+default[\"phpapp\"][\"db_username\"] = \"phpapp\"
+" > phpapp/attributes/default.rb
+
+echo "{
+  \"mysql\": {\"server_root_password\": \"808052769e2c6d909027a2905b224bad\", \"server_debian_password\": \"569d1ed2d46870cc020fa87be83af98d\", \"server_repl_password\": \"476911180ee92a2ee5a471f33340f6f4\"},
+  \"phpapp\": {\"db_password\": \"212b09752d173876a84d374333ae1ffe\"},
+  \"run_list\": [ \"recipe[apt]\", \"recipe[phpapp]\" ]
+}" > web.json
+
+
+echo "
+#
+# Cookbook Name:: phpapp
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+include_recipe \"apache2\"
+include_recipe \"mysql::client\"
+include_recipe \"mysql::server\"
+include_recipe \"php\"
+include_recipe \"php::module_mysql\"
+include_recipe \"apache2::mod_php5\"
+include_recipe \"mysql::ruby\"
+
+apache_site \"default\" do
+  enable true
+end
+
+mysql_database node['phpapp']['database'] do
+  connection ({:host => 'localhost', :username => 'root', :password => node['mysql']['server_root_password']})
+  action :create
+end
+
+mysql_database_user node['phpapp']['db_username'] do
+  connection ({:host => 'localhost', :username => 'root', :password => node['mysql']['server_root_password']})
+  password node['phpapp']['db_password']
+  database_name node['phpapp']['database']
+  privileges [:select,:update,:insert,:create,:delete]
+  action :grant
+end
+
+
+wordpress_latest = Chef::Config[:file_cache_path] + \"/wordpress-latest.tar.gz\"
+
+remote_file wordpress_latest do
+  source \"http://wordpress.org/latest.tar.gz\"
+  mode \"0644\"
+end
+
+directory node[\"phpapp\"][\"path\"] do
+  owner \"root\"
+  group \"root\"
+  mode \"0755\"
+  action :create
+  recursive true
+end
+
+execute \"untar-wordpress\" do
+  cwd node['phpapp']['path']
+  command \"tar --strip-components 1 -xzf \" + wordpress_latest
+  creates node['phpapp']['path'] + \"/wp-settings.php\"
+end
+
+ 
+" > phpapp/recipes/default.rb
+
+
+echo "
+default[\"phpapp\"][\"database\"] = \"phpapp\"
+default[\"phpapp\"][\"db_username\"] = \"phpapp\"
+default[\"phpapp\"][\"path\"] = \"/var/www/phpapp\"
+" > phpapp/attributes/default.rb
+
+
+
+IN THE 'Templates' section on the we page
+
+
